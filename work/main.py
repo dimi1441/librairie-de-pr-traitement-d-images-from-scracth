f632046fea038ec6aff92ed631f8889400ff9c24
Dimitri-Kamdem <dkamdem26@gmail.com>
2020-09-01 01:12:06 +0100

    Ending

--- work/main.py
+++ work/main.py
@@ -4,15 +4,5 @@ import Image
 import numpy as np
 
 
-filter = np.transpose(np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]]))
-
-im2 = Image.Image("../../../data/input/lena.pgm")
-#im3 = im2.multiplication(1.5, "mult.pgm")
-
-filter = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]]) 
-
-im2.histogram_equalization("im.pgm")
-
-#im2.histogram("hist.pdf")
-
-#im2.intensity_profil((200, 400), (200, 100), 5, "result.pgm", "intensity.pdf")
+######################################################################################
+#########################  You can edit your code Here ###############################
